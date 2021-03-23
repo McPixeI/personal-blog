5791e662b6c9ffb6d54a64e63ea5e9e776f05feb
McPixeI <torres.88.bcn@gmail.com>
2021-03-23 22:42:08 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,8 +10,12 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>Lo siento, la página que buscas no existe...</p>
+      <div className="not-found">
+        <h1 className='not-found__title'>Error <span>404</span></h1>
+        <p>Lo siento, la página que buscas no existe...</p>
+        <p>Puedes buscar cualquier otro artículo en el siguiente enlace:</p>
+        <Link to="/blog" class="btn btn--primary">Ver artículos</Link>
+      </div>
     </Layout>
   )
 }
