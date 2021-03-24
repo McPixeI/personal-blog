eab279f29a0f6a2861e30b85220ce8b806bcb686
McPixeI <torres.88.bcn@gmail.com>
2021-03-24 23:45:07 +0100

    added user feedback to newsletter subscription

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,42 +1,60 @@
 import React, { useState } from "react"
 import addToMailchimp from 'gatsby-plugin-mailchimp'
+import messages from '../data/messages'
 
 const Newsletter = () => {
-    const [email, setEmail] = useState('')
+
+    const [formState, setFormState] = useState({
+            state: '',
+            email: '',
+            title: messages.newsletter.initial.title,
+            msg: messages.newsletter.initial.text
+        })
 
     const handleInputChange = evt => {
-       setEmail(evt.target.value)
+        setFormState({
+            ...formState,
+            email: evt.target.value
+        })
     }
 
     const handleSubmit =  evt => {
         evt.preventDefault();
-        addToMailchimp(email)
-        .then(data => {
-            console.log(data)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-        // I recommend setting `result` to React state
-        // but you can do whatever you want
+        addToMailchimp(formState.email)
+            .then(data => {
+                if (data.result === "error") {
+                    setFormState({
+                        ...formState,
+                        state: 'error',
+                        title: messages.newsletter.error.title,
+                        msg: messages.newsletter.error.text,
+                        email:''
+                    })
+                } else {
+                    setFormState({
+                        ...formState,
+                        state: 'success',
+                        title: messages.newsletter.succes.title,
+                        msg: messages.newsletter.succes.text,
+                        email:''
+                    })
+                }
+            })
       }
 
     return (
         <div className="newsletter">
-            <h3>¡Suscríbete!</h3>
-            <p>
-                Entérate al momento cada vez que publique un artículo que pueda interesarte.
-                Tranquil@, <strong>no recibirás spam</strong> 😊.
-            </p>
+            <h3>{formState.title}</h3>
+            <p className={formState.state}>{formState.msg}</p>      
             <form onSubmit={handleSubmit}>
                 <input className="newsletter__input"
                     type="text"
                     aria-label="Subscribe"
                     placeholder="Tu correo..."
-                    value={email}
+                    value={formState.email}
                     onChange={handleInputChange}
                 />
-                <button className="btn btn--primary newsletter__btn" type="submit">Me suscribo!</button>
+                <button className="btn btn--primary newsletter__btn" type="submit">¡Me suscribo!</button>
             </form>
         </div>
         
