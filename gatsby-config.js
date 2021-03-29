75e94fc07a2c3f535044c0d2c2de854467636e29
McPixeI <torres.88.bcn@gmail.com>
2021-03-29 12:34:10 +0200

    -

--- gatsby-config.js
+++ gatsby-config.js
@@ -147,7 +147,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#2d688b`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.jpg`, // This path is relative to the root of the site.
+        icon: `src/images/logo.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
